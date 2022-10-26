5cc0b3aa2d411cbf8be85628b27f160e4481489a
AndyJado <101876416+AndyJado@users.noreply.github.com>
2022-10-26 20:58:34 +0800

    pass test 4

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,12 @@
-use proc_macro::TokenStream;
+use proc_macro2::{Ident, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let inpu = parse_macro_input!(input as DeriveInput);
     let name = inpu.ident;
+    let belly = builder(&inpu.data);
     let expaned = quote! {
         pub struct CommandBuilder {
             executable: Option<String>,
@@ -30,6 +31,18 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 self.current_dir = Some(current_dir);
                 self
             }
+            fn wa(&mut self) {
+                let s = stringify!(#belly);
+                println!("{:?}", s);
+            }
+            fn build(mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                let executable = self.executable.unwrap();
+                let args = self.args.unwrap();
+                let env = self.env.unwrap();
+                let current_dir = self.current_dir.unwrap();
+                //FIXME: quote!
+                Ok(#name {#belly})
+            }
         }
         impl #name {
             pub fn builder() -> CommandBuilder {
@@ -42,5 +55,21 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         }
     };
-    TokenStream::from(expaned)
+    proc_macro::TokenStream::from(expaned)
+}
+
+fn builder(data: &Data) -> TokenStream {
+    match *data {
+        Data::Struct(ref data) => match data.fields {
+            syn::Fields::Named(ref fields) => {
+                // FIXME: how to return unwraped?
+                let recurse = fields.named.iter().map(|f| f.ident.clone().unwrap());
+                quote! {
+                    #(#recurse:#recurse),*
+                }
+            }
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    }
 }

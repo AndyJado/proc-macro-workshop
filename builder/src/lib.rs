11c8583a0028f319020d3231dc25cf5574cb88a5
AndyJado <101876416+AndyJado@users.noreply.github.com>
2022-10-27 18:50:12 +0800

    member_stack

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -9,29 +9,16 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let builder_name = format_ident!("{}Builder", name);
     let belly = builder(&inpu.data);
     let builder_belly = builder_body(&inpu.data);
+    let builder_field_init = builder_body_init(&inpu.data);
+    let implied = member_stact(&inpu.data);
     let expaned = quote! {
         pub struct #builder_name {#builder_belly}
         impl #builder_name {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
             fn wa(&mut self) {
                 let s = stringify!(#belly);
                 println!("{:?}", s);
             }
+            #implied
             fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
                 let executable = self.executable.take().unwrap();
                 let args = self.args.take().unwrap();
@@ -42,12 +29,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
         impl #name {
             pub fn builder() -> #builder_name {
-                #builder_name {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
-                }
+                #builder_name { #builder_field_init }
             }
         }
     };
@@ -66,12 +48,36 @@ fn struct_data(cb: &FieldsFinger, data: &Data) -> TokenStream {
     }
 }
 
+fn member_stact(data: &Data) -> TokenStream {
+    let cb: Box<FieldsFinger> = Box::new(|fields| {
+        let recurse = fields.named.iter().map(|f| &f.ident);
+        let recurse_ty = fields.named.iter().map(|f| &f.ty);
+        quote! {
+            #(fn #recurse(&mut self, #recurse: #recurse_ty) -> &mut Self {
+                self.#recurse = Some(#recurse);
+                self
+            })*
+        }
+    });
+    struct_data(&cb, data)
+}
+
 fn builder_body(data: &Data) -> TokenStream {
     let cb: Box<FieldsFinger> = Box::new(|fields| {
         let recurse = fields.named.iter().map(|f| &f.ident);
         let recurse_ty = fields.named.iter().map(|f| &f.ty);
         quote! {
             #(#recurse : Option<#recurse_ty>),*
+        }
+    });
+    struct_data(&cb, data)
+}
+
+fn builder_body_init(data: &Data) -> TokenStream {
+    let cb: Box<FieldsFinger> = Box::new(|fields| {
+        let recurse = fields.named.iter().map(|f| &f.ident);
+        quote! {
+            #(#recurse : None),*
         }
     });
     struct_data(&cb, data)

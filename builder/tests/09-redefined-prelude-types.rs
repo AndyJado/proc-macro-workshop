36c48e2a67c9f19380b97d4df24c44ccf256d771
AndyJado <101876416+AndyJado@users.noreply.github.com>
2022-11-02 15:48:18 +0800

    mark

--- builder/tests/09-redefined-prelude-types.rs
+++ builder/tests/09-redefined-prelude-types.rs
@@ -10,9 +10,9 @@
 // different from Result::Ok. This caused problems in code doing `use hyper::*`
 // together with macro-generated code referring to Ok.
 //
-// Generally all macros (procedural as well as macro_rules) designed to be used
+// *Generally all macros (procedural as well as macro_rules) designed to be used
 // by other people should refer to every single thing in their expanded code
-// through an absolute path, such as std::result::Result.
+// through an absolute path, such as std::result::Result.*
 
 use derive_builder::Builder;
 
